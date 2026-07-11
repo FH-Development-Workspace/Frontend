3b455710df6c6ae57d1565dd485fa767adb98bf0
fisteywol <williamoystoniscool12@gmail.com>
2026-07-11 20:10:11 +0100

    d

---
+++ interaction/interactive.js
@@ -0,0 +1,11 @@
+function returnToWeb() {
+    window.location.href = "https://logs.fh-development.xyz/";
+}
+
+function forwardSite(siteURI) {
+    window.location.href = siteURI;
+}
+
+function showAlert(message) {
+    alert(message);
+}
